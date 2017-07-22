a7ddd5c988a833dae91c1f42785af7f21b7c89b8
Angular CLI <angular-cli@angular.io>
2017-07-22 11:11:19 +0200

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { PonyracerPage } from './app.po';
+
+describe('ponyracer App', () => {
+  let page: PonyracerPage;
+
+  beforeEach(() => {
+    page = new PonyracerPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to pr!');
+  });
+});
